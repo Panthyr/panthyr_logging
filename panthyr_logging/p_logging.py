966ae3cc88d288d724cdcf61a1291a05455f0f98
dietervansteenwegen <github@vansteenwegen.org>
2022-05-11 14:38:50 +0200

    Add milliseconds formatter, db handler

--- panthyr_logging/p_logging.py
+++ panthyr_logging/p_logging.py
@@ -6,61 +6,82 @@ __email__ = 'dieter.vansteenwegen@vliz.be'
 __project__ = 'Panthyr'
 __project_link__ = 'https://waterhypernet.org/equipment/'
 import logging
-# import logging.handlers
+import logging.handlers
+import datetime as dt
+from typing import Union
 
-FMT = '%(asctime)s |%(levelname)-7s |%(module)-10.10s|%(lineno)-03s |%(funcName)s |%(message)s'
-DATEFMT = '%d/%m/%Y %H:%M:%S'  # defines format for timestamp
+LOG_FMT = '%(asctime)s|%(levelname)-7.7s|%(module)-10.10s|%(lineno)-0.3d|%(funcName)s|%(message)s'
+# See https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+DATE_FMT = '%d/%m/%Y %H:%M:%S'
+LOGFILE = './panthyr_log.log'
+LOGMAXBYTES = 500000
 
 
-def setup_logging(email: bool = False, to_db: bool = True) -> logging.Logger:
+class MilliSecondsFormatter(logging.Formatter):
+
+    def formatTime(self, record: logging.LogRecord, datefmt: Union[str, None] = None) -> str:
+        # sourcery skip: lift-return-into-if, remove-unnecessary-else
+        creation_time = dt.datetime.fromtimestamp(record.created)
+        if datefmt:
+            s = creation_time.strftime(datefmt)
+        else:
+            t = creation_time.strftime(DATE_FMT)
+            s = f'{t}.{int(record.msecs)}'
+        return s
+
+
+def setup_logger(name: Union[str, None] = None) -> logging.Logger:
     """Setup logging.
 
     Returns logger object with (at least) 1 streamhandler to stdout.
-    Additionally, it will log to the database and/or email for
+    Base logger level is set to logging.DEBUG
+    Streamhandler level is set to logging.ERROR
+    Uses the MilliSecondsFormatter to add milliseconds to the timestamp.
 
     Args:
-        email (bool, optional): Send email for critical logs (>= warning). Defaults to False.
-        to_db (bool, optional): Add logs to database. Defaults to True.
+        name (Union[str, None], optional): Name of the logger. None to get the root logger.
+                Defaults to None.
 
     Returns:
-        logging.Logger: configured logger object
+        logging.Logger: configured logger object with streamhandler
     """
 
-    log = logging.getLogger(__name__)  # creates root logger with name of script/module
-    log.setLevel(logging.DEBUG)
+    logger = logging.getLogger() if name is None else logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-    h1 = logging.StreamHandler()  # handler to stdout for less important messages
-    h1.setLevel(logging.DEBUG)
-    h1.setFormatter(logging.Formatter(FMT, DATEFMT))
-    log.addHandler(h1)  # add both handlers to the logger
-
-    if to_db or email:
-        try:
-            from panthyr_db.p_db import pDB
-            db = pDB()
-
-        except Exception as e:
-            print(f'exception: {e}')
-            return log
-    else:
-        db = None
-
-    if to_db:
-        from .p_handlers import db_Handler
-        h3 = db_Handler(db)
-        h3.setLevel(logging.DEBUG)
-        log.addHandler(h3)
-
-    if email:
-        setup_email(log, db)
-
-    return log
+    stream_handler = logging.StreamHandler()  # handler to stdout
+    stream_handler.setLevel(logging.ERROR)
+    stream_handler.setFormatter(MilliSecondsFormatter(LOG_FMT))
+    logger.addHandler(stream_handler)
+    return logger
 
 
-def setup_email(log: logging.Logger, db) -> logging.Logger:
-    """Set up the email handler.
+def add_rotating_file_handler(logger: logging.Logger) -> None:
+    """Add a rotating file handler to the logger.
 
-    Gets credentials from the credentials module and recipient from the database.
+    Uses LOGFILE constant for the base file name and LOGMAXBYTES for the rollover size.
+    Handler level is set to logging.DEBUG.
+
+    Args:
+        logger (logging.Logger): Logger to add the handler to.
+    """
+    rot_file_handler = logging.handlers.RotatingFileHandler(
+        LOGFILE,
+        maxBytes=LOGMAXBYTES,
+        backupCount=3,
+    )
+    rot_file_handler.doRollover()
+    rot_file_handler.setLevel(logging.DEBUG)
+    rot_file_handler.setFormatter(MilliSecondsFormatter(LOG_FMT))
+    logger.addHandler(rot_file_handler)
+
+
+def add_email_handler(logger: logging.Logger, db) -> None:
+    """Adds and configures email handler.
+
+    Adds the buffered_SMTP_Handler to the logger.
+    Gets (login) credentials and server from the credentials module, recipient from the database.
+    Handler level is set to logging.WARNING
 
     Args:
         log (logging.Logger): logger object to add handler to
@@ -71,26 +92,43 @@ def setup_email(log: logging.Logger, db) -> logging.Logger:
     """
     from .p_handlers import buffered_SMTP_Handler
     try:
-        if db.get_setting('email_enabled') == 1:
-            from panthyr_credentials.p_credentials import pCredentials
-            cred = pCredentials()
-            server_port = cred.get_cred('email_server_port')
-            user = cred.get_cred('email_user')
-            password = cred.get_cred('email_password')
+        from panthyr_credentials.p_credentials import pCredentials
+        cred = pCredentials()
+        server_port = cred.get_cred('email_server_port')
+        user = cred.get_cred('email_user')
+        password = cred.get_cred('email_password')
 
-            station_id = db.get_setting('station_id')
-            recipient = db.get_setting('email_recipient')
-            h2 = buffered_SMTP_Handler(
-                host=server_port,
-                fromaddress=user,
-                toaddress=recipient,
-                password=password,
-                station_id=station_id,
-            )  # handler for messages that should be emailed
-            h2.setLevel(logging.WARNING)
-            h2.setFormatter(logging.Formatter(FMT, DATEFMT))
-            log.addHandler(h2)
+        station_id = db.get_setting('station_id')
+        recipient = db.get_setting('email_recipient')
+        h2 = buffered_SMTP_Handler(
+            host=server_port,
+            fromaddress=user,
+            toaddress=recipient,
+            password=password,
+            station_id=station_id,
+        )  # handler for messages that should be emailed
+        h2.setLevel(logging.WARNING)
+        h2.setFormatter(MilliSecondsFormatter(LOG_FMT))
+        logger.addHandler(h2)
     except Exception:
-        return log
+        logger.exception('Could not add email handler.')
 
-    return log
+
+def add_database_handler(logger: logging.Logger, db) -> None:
+    """Adds and configures database handler.
+
+    Adds the database logs table to the logger output.
+    Gets (login) credentials and server from the credentials module, recipient from the database.
+    Handler level is set to logging.WARNING
+
+    Args:
+        log (logging.Logger): logger object to add handler to
+        db (_type_): Panthyr database
+
+    Returns:
+        logging.Logger: logger with added email handler
+    """
+    from .p_handlers import db_Handler
+    h3 = db_Handler(db)
+    h3.setLevel(logging.DEBUG)
+    logger.addHandler(h3)
