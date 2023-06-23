48b4a7911846c87569b41a778e068158dfc88888
Panthyr <panthyr@example.com>
2023-06-23 13:18:56 +0000

    nicer formatting

--- panthyr_logging/p_handlers.py
+++ panthyr_logging/p_handlers.py
@@ -54,9 +54,9 @@ class buffered_SMTP_Handler(logging.handlers.BufferingHandler):
                            f'Subject: {self.subject}\r\n\r\n'
         mailbody = ''
         for log in self.buffer:
-            mailbody += '*' * 40 + '\r\n'
+            mailbody += f'\r\n{"*" * 40}\r\n'
             log_str = self.format(log)
-            log_str = '\r\n'.join([line for line in log_str.split('\r\n') if line != ''])
+            log_str = '\r\n|'.join([line for line in log_str.split('\n')])
             mailbody += f'{log_str}\r\n'
 
         try:
